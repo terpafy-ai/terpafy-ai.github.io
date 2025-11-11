11bdfea8d823d5390c7781f4b3e99f4d099817f2
Henrique Hirako <henriquehirako@gmail.com>
2025-11-10 23:01:41 -0500

    feat: Add GitHub Actions workflow for automated deployment (#2)
    * Cleanup
    
    * feat: add GitHub Actions workflow for automated deployment
    
    * deploy me
    
    * fix: set base path to root for custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/terpafy-lp/' : '/'
-  
-  return {
-    base: base,
-    plugins: [react()],
-    server: {
-      port: 5173,
-      host: true
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: true,
-      assetsDir: 'assets'
-    }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    assetsDir: 'assets'
   }
 })
